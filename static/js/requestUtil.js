3317db5947bb4a4e0ea500cbe5ed86f8f4b13891
lmwis <detectiveygn@163.com>
2021-03-29 18:00:57 +0800

    feat<login>: finish login framework

---
+++ static/js/requestUtil.js
@@ -0,0 +1,18 @@
+var request = (function () {
+    var req;
+    req.login = function () {
+        // $.ajax({
+        //     url:hostPre+"api/v1/admin/login",
+        //     type:"post",
+        //     data:{
+        //         username:data.field.username,
+        //         password:data.field.password
+        //     },
+        //     success:function (res) {
+        //         console.log(res);
+        //         // location.href='./index.html'
+        //     }
+        // })
+    };
+    return req;
+})(request);
