27fa7ad0fb27669168b3868c9d2c76c3f5858fe7
lmwis <detectiveygn@163.com>
2021-03-29 20:39:44 +0800

    feat<login>: finish list and edit page

--- static/js/global.js
+++ static/js/global.js
@@ -1,1 +1,7 @@
-var hostPre = "http://10.111.217.34:11111/";
+var hostPre = "http://10.111.62.80:8000/";
+var b64EncodeUnicode =  function(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+        function(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+}
